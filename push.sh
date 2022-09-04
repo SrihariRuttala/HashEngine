d1f78e24b2d19befd3c05dff248a177b93ff09f7
Srihari Ruttala <45734841+SrihariRuttala@users.noreply.github.com>
2022-09-05 00:27:46 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "# HashEngine" >> README.md
-git init
-git add .
-git commit -m "Push source"
-git branch -M main
-git remote add origin git@github.com:SrihariRuttala/HashEngine.git
-git push -u origin main
